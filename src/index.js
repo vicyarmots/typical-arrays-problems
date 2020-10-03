d21f6cda27485d5204904c7b5f14ea9c87e11718
vicyarmots <victor.yarmots@yandex.ru>
2020-10-03 20:28:57 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    let res = 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (Array.isArray(array) && array.length !== 0) {
+        array.reduce(function (preVal, Val) {
+            res = preVal < Val ? preVal : Val;
+            return res;
+        });
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return res;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let res = 0;
+
+    if (Array.isArray(array) && array.length !== 0) {
+        array.reduce(function (preVal, Val) {
+            res = preVal > Val ? preVal : Val;
+            return res;
+        });
+    }
+
+    return res;
+};
+
+exports.avg = function avg(array) {
+    let res = 0;
+
+    if (Array.isArray(array) && array.length !== 0) {
+        array.map((item) => (res += item));
+        let avg = res / array.length;
+        return avg;
+    }
+
+    return res;
+};
